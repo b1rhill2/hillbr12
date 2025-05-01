f8d450bd6ce535e782a324319fbedc850767726e
b1rhill2 <hillbr12@msu.edu>
2025-05-01 13:33:44 -0400

    Add Procfile for render deployment

--- Homework-3/flask_app/routes.py
+++ Homework-3/flask_app/routes.py
@@ -154,9 +154,4 @@ def resume():
 # def static_dir(path):
 #     return send_from_directory("static", path)
 
-@app.after_request
-def add_header(r):
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    return r
+s.html
